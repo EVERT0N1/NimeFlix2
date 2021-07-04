edbd73287228fc887acb35d9dfca6c32e7ad7a65
Everton <onyplayer@hotmail.com>
2021-07-04 20:21:16 -0300

    primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        1300:{
+            items:7
+        }
+    }
+})
+
